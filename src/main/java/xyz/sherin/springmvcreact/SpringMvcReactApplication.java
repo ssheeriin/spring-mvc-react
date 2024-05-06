92eaff2797319ad5de27a42f875d6bbefee90f1d
sherin <mail@sherin.xyz>
2024-05-05 19:48:56 -0700

    Initial commit

---
+++ src/main/java/xyz/sherin/springmvcreact/SpringMvcReactApplication.java
@@ -0,0 +1,13 @@
+package xyz.sherin.springmvcreact;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMvcReactApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpringMvcReactApplication.class, args);
+  }
+
+}
---
+++ src/test/java/xyz/sherin/springmvcreact/SpringMvcReactApplicationTests.java
@@ -0,0 +1,13 @@
+package xyz.sherin.springmvcreact;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMvcReactApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
